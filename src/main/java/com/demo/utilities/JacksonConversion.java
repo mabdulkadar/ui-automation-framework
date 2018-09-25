f8d064c057305ae33274c6cf22c61d63753a118c
mohd.abdul <mohamed.abdulkadar@groupm.com>
2018-09-24 23:47:44 -0400

    1) Demo Api Test examples

--- src/main/java/com/demo/utilities/JacksonConversion.java
+++ src/main/java/com/demo/utilities/JacksonConversion.java
@@ -1,11 +1,10 @@
 package com.demo.utilities;
 
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
